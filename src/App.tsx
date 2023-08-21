0dc525dde0317514adc86789ddff8437431b2d33
devtodo <luciano.devtodo@gmail.com>
2023-08-21 11:52:24 -0300

    sort users by country

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ import UsersTable from "./components/UsersTable";
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [toggleColors, setToggleColors] = useState(false);
+  const [sortByCountry, setSortByCountry] = useState(false);
+
+  const toggleSortByCountry = () => {
+    setSortByCountry((prevState) => !prevState);
+  };
 
   useEffect(() => {
     fetch("https://randomuser.me/api?results=100")
@@ -14,6 +19,12 @@ function App() {
       .catch((err) => console.error(err));
   }, []);
 
+  const sortedUsers = sortByCountry
+    ? users.toSorted((a, b) => {
+        return a.location.country.localeCompare(b.location.country);
+      })
+    : users;
+
   return (
     <div>
       <h1>Users List</h1>
@@ -21,9 +32,12 @@ function App() {
         <button onClick={() => setToggleColors(!toggleColors)}>
           Toggle Colors
         </button>
+        <button onClick={toggleSortByCountry}>
+          {sortByCountry ? "Order Random" : "Order by country"}
+        </button>
       </header>
       <main>
-        <UsersTable users={users} showColors={toggleColors} />
+        <UsersTable users={sortedUsers} showColors={toggleColors} />
       </main>
     </div>
   );
