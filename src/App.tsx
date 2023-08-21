d5e5782b6827025304a1774f17d3ab3cb647a3b4
devtodo <luciano.devtodo@gmail.com>
2023-08-21 19:32:22 -0300

    filter by country

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import './App.css'
 import { type User } from './types'
 import UsersTable from './components/UsersTable'
 
 function App() {
+  const originalUSers = useRef<User[]>([])
   const [users, setUsers] = useState<User[]>([])
   const [toggleColors, setToggleColors] = useState(false)
   const [sortByCountry, setSortByCountry] = useState(false)
-  const originalUSers = useRef<User[]>([])
+  const [filterCountry, setFilterCountry] = useState<string | undefined>(
+    undefined
+  )
 
   const toggleSortByCountry = () => {
     setSortByCountry((prevState) => !prevState)
@@ -35,11 +38,23 @@ function App() {
       })
   }, [])
 
-  const sortedUsers = sortByCountry
-    ? users.toSorted((a, b) =>
-        a.location.country.localeCompare(b.location.country)
-      )
-    : users
+  const filteredUsers = useMemo(() => {
+    return typeof filterCountry === 'string' && filterCountry.length > 0
+      ? users.filter((user) =>
+          user.location.country
+            .toLocaleLowerCase()
+            .includes(filterCountry.toLocaleLowerCase())
+        )
+      : users
+  }, [users, filterCountry])
+
+  const sortedUsers = useMemo(() => {
+    return sortByCountry
+      ? filteredUsers.toSorted((a, b) =>
+          a.location.country.localeCompare(b.location.country)
+        )
+      : filteredUsers
+  }, [filteredUsers, sortByCountry])
 
   return (
     <div>
@@ -56,6 +71,12 @@ function App() {
           {sortByCountry ? 'Order Random' : 'Order by country'}
         </button>
         <button onClick={resetUsers}>Get initial state</button>
+        <input
+          type="text"
+          onChange={(e) => setFilterCountry(e.target.value)}
+          value={filterCountry}
+          placeholder="Filter by country"
+        />
       </header>
       <main>
         <UsersTable
